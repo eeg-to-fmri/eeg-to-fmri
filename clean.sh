c0a103556ea21069c724b687240269628682b9a0
DCalhas <daivdcalhas@hotmail.com>
2022-12-16 10:53:16 +0000

    example notebook for EEG to fMRI synthesis

---
+++ clean.sh
@@ -0,0 +1,11 @@
+
+
+rm -r src/eeg_to_fmri/__pycache__/
+rm -r src/eeg_to_fmri/regularizers/__pycache__/
+rm -r src/eeg_to_fmri/utils/__pycache__/
+rm -r src/eeg_to_fmri/models/__pycache__/
+rm -r src/eeg_to_fmri/data/__pycache__/
+rm -r src/eeg_to_fmri/learning/__pycache__/
+rm -r src/eeg_to_fmri/layers/__pycache__/
+rm -r src/eeg_to_fmri/explainability/__pycache__/
+rm -r src/eeg_to_fmri/metrics/__pycache__/
